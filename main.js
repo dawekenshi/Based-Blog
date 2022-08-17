a53f100372a6b9c660a54aa7218fcd993887243e
dawekenshi <104755414+dawekenshi@users.noreply.github.com>
2022-08-17 12:24:41 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function spanish(){
+    document.querySelector('.title').textContent = "Bienvenido a mi blog!"
+    document.querySelector('.main-text').textContent = "Q: ¿Que encontraras en esta pagina?"
+    document.querySelector('.main-text2').textContent = "Q: ¿Porque fue esta pagina creada?"
+    document.querySelector('.main-text3').textContent = "Q: ¿Quien soy yo?"
+    document.querySelector('.answer').textContent = "A: En esta pagina encontraras tutoriales sobre herramientas de GNU/Linux o Windows o my opinion sobre ciertas distribuciones que pruebe."
+    document.querySelector('.answer2').textContent = "A: Fue creada para practicar mientras aprendo desarrollo web, mas tratando de enseñar mientras expando mi conocimiento sobre GNU/Linux."
+    document.querySelector('.answer3').textContent = "A: Soy un chico de 14 años en processo de ser un programador autodidacta, no solo interesado en desarollo web full stack sino tambien en automatizacion y web scrapping."
+    document.querySelector('.scroll').textContent = "Redes sociales y Monero ⬇"
+    document.querySelector('.text-footer').textContent = "Gracias por visitar mi pagina :)"
+}
+
+function english(){
+    document.querySelector('.title').textContent = "Welcome to my blog!"
+    document.querySelector('.main-text').textContent = "Q: What you will find in this page?"
+    document.querySelector('.main-text2').textContent = "Q: Why was this page created?"
+    document.querySelector('.main-text3').textContent = "Q: Who am I?"
+    document.querySelector('.answer').textContent = "A: In this page you will find tutorials about GNU/Linux or Windows tools or my opinion about certain distros that I try."
+    document.querySelector('.answer2').textContent = "A: Just me trying to practice while learning web dev, plus trying to teach and expand my GNU/Linux knowledge."
+    document.querySelector('.answer3').textContent = "A: I am a 14 y/o boy in proccess of being a self thaught programmer, not only interested in web dev full stack but also interested in automation and web scrapping."
+    document.querySelector('.scroll').textContent = "Social media and Monero ⬇"
+    document.querySelector('.text-footer').textContent = "Thanks for visiting my page :)"
+}
